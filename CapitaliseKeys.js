ab6a6b8c2f26280308dcb75d09b5a758f483b2d0
PJSalter <psalter88@googlemail.com>
2021-12-10 04:28:34 +0000

    added objects code solution

---
+++ CapitaliseKeys.js
@@ -0,0 +1,20 @@
+//Capitalise keys 
+//Write a function capitaliseKeys, which takes an object as an argument, and returns a new object with the same key-value pairs except all the keys have been capitalised.
+
+//Attempt 1
+
+function capitaliseKeys(obj) {
+    var keys = Object.keys(obj)
+    console.log(keys);
+    return keys.reduce((acc, key) => {
+      acc[key.toUpperCase()] = obj[key];
+    return acc
+    }, {});
+  }
+
+
+console.log(capitaliseKeys({ a: 1, b: 2, c: 3 })); //Expected: { A: 1, B: 2, C: 3 }
+
+console.log(capitaliseKeys({ Hello: "hi" })); //Expected: { HELLO: 'hi' }
+
+console.log(capitaliseKeys({})); //Expected: {}
